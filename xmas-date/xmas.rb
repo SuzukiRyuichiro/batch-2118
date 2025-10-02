d35a4981bfef174d650d3b1f24bdcc2b785efe17
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-10-02 22:04:44 +0900

    make it easier to read

--- xmas-date/xmas.rb
+++ xmas-date/xmas.rb
@@ -5,18 +5,18 @@ def xmas
   # get today's date in Date object
   today = Date.today
 
-  # get the next xmas date in Date object
+  # get the this year's xmas date in Date object
   this_year_xmas_date = Date.new(today.year, 12, 25)
 
-  # if today is passed 12/25, set the year to the next year
+  # if today is past 12/25, set the year to the next year's xmas
   xmas_date = if this_year_xmas_date < today
                 Date.new(today.year + 1, 12, 25)
               else
+                # else get this year's xmas
                 Date.new(today.year, 12, 25)
               end
-  # else get today.year
 
-  # subtract the today's date from xmas date (Rational)
+  # subtract the today's date from xmas date (we get Rational)
   # convert the days in rational into integer
   (xmas_date - today).to_i
 end
