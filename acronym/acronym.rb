b8a2fe69831f30811e847a6a114ab7a5e743983f
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-10-04 17:51:03 +0900

    acronym done

---
+++ acronym/acronym.rb
@@ -0,0 +1,28 @@
+def acronym(sentence) # 'what the fuck'
+  # TODO: make the sentence into an acronym
+  # separate the words into an array ['what', 'the', 'fuck']
+  words = sentence.split
+  result = ''
+  # start iterating over each words
+  for word in words
+    # on each iteration, get the first character of the word
+    first_character = word[0]
+    # capitalize the character, concatenate into a single acronymized string
+    result += first_character.upcase
+  end
+  result
+end
+
+def acronym_v2(sentence)
+  # separate the words into an array ['what', 'the', 'fuck']
+  words = sentence.split
+  first_characters = []
+  # start iterating over each words
+  words.each do |word|
+    # put the first character (upcased) of the word into first characters array
+    first_characters << word[0].upcase
+  end
+
+  # join them later
+  first_characters.join
+end
