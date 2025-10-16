da8675ed26f489625c815e8a57debb9118fb80e7
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-10-16 22:06:02 +0900

    done with reboot and regex

--- regex-live/parse_url.rb
+++ regex-live/parse_url.rb
@@ -1,4 +1,13 @@
-def parse_url(_url)
-  { scheme: 'http', subdomain: 'www', domain: 'lewagon', top_level_domain: '.com',
-    path: [], params: {} }
+def parse_url(url)
+  # write regex to capture some part of the URL
+  match_data = url.match(/(?<scheme>^https?):\/\/(?<subdomain>\w+)\.(?<domain>[\w-]+)(?<top_level_domain>\.\w{2,10})/)
+
+  {
+    scheme: match_data[:scheme],
+    subdomain: match_data[:subdomain],
+    domain: match_data[:domain],
+    top_level_domain: match_data[:top_level_domain],
+    params: {},
+    path: []
+  }
 end
