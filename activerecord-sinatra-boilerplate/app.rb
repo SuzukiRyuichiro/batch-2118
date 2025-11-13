ea8c02f03a42fc994a3c36230db63da970d26b87
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-11-13 21:24:12 +0900

    added restaurant model, added migration, added seed

--- activerecord-sinatra-boilerplate/app.rb
+++ activerecord-sinatra-boilerplate/app.rb
@@ -1,8 +1,12 @@
-require_relative "config/application"
-require "sinatra"
-require "sinatra/reloader"
-require "sinatra/activerecord"
+require_relative 'config/application'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/activerecord'
 
-get "/" do
-  "Hello world!"
+get '/' do
+  'Hey everyone!'
+end
+
+get '/restaurants' do
+  'TODO: list all the restaurants in the DB'
 end
---
+++ activerecord-sinatra-boilerplate/db/seeds.rb
@@ -0,0 +1,3 @@
+Restaurant.create(name: 'Jackrabbit slims', address: 'Los Angels', category: 'American', capacity: 100)
+Restaurant.create(name: 'Udon Palace', address: 'Okinawa', category: 'Japanese', capacity: 15)
+Restaurant.create(name: 'We are the farm', address: 'Meguro', category: 'Vegetarian', capacity: 50)
