7ec325e2126ef48ce46b8c65511f472aabcddd38
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-12-05 18:59:06 +0900

    done

--- http-and-api-lecture-boilerplate/lib/index.js
+++ http-and-api-lecture-boilerplate/lib/index.js
@@ -1,2 +1,20 @@
 // TODO: Fetch an activity with the Bored API - Let's pseudocode!
-const url = "https://bored.api.lewagon.com/api/activity/";
+const url = "https://the-fork.api.lewagon.com/api/v1/restaurants";
+
+const button = document.querySelector("#button");
+const list = document.querySelector("#restaurants");
+
+button.addEventListener("click", () => {
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      data.forEach((restaurant) => {
+        const li = document.createElement("li");
+        li.classList.add("list-group-item");
+        li.innerText = `${restaurant.name} - 🏠: ${restaurant.address}`;
+        list.appendChild(li);
+      });
+    });
+});
