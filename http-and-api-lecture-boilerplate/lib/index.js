ad6036d315a04b968d0e38735296cc36b4e5aefb
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2025-12-05 19:00:01 +0900

    updat the lecture

--- http-and-api-lecture-boilerplate/lib/index.js
+++ http-and-api-lecture-boilerplate/lib/index.js
@@ -4,17 +4,4 @@ const url = "https://the-fork.api.lewagon.com/api/v1/restaurants";
 const button = document.querySelector("#button");
 const list = document.querySelector("#restaurants");
 
-button.addEventListener("click", () => {
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      data.forEach((restaurant) => {
-        const li = document.createElement("li");
-        li.classList.add("list-group-item");
-        li.innerText = `${restaurant.name} - 🏠: ${restaurant.address}`;
-        list.appendChild(li);
-      });
-    });
-});
+// TODO: when clicked on a button, populate the restaurant list with restaurants you get from the API
