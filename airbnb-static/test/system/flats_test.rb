0732e86bb9635ed425d46f13bcb0f921670f5908
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2026-01-08 22:03:14 +0900

    done

--- airbnb-static/test/system/flats_test.rb
+++ airbnb-static/test/system/flats_test.rb
@@ -2,7 +2,7 @@ require "application_system_test_case"
 
 class FlatsTest < ApplicationSystemTestCase
   test "visiting the index" do
-    visit root_path
+    visit "/"
 
     assert_selector "h1", text: "Flats"
     assert_selector "ul > li", count: 6
